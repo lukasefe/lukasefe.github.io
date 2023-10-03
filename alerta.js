86dd300e03b0158f3704545db040b72367e8280b
lukasefe <113307417+lukasefe@users.noreply.github.com>
2023-10-03 08:59:49 -0300

    03/10/2023 - V 2.4.02
    Se agrego prepración de la infusión continua de la insulina CTE

--- alerta.js
+++ alerta.js
@@ -1,1 +1,1 @@
-window.alert("29/09/2023 - V 2.4.01: Se actualizarón datos consignados a la preparacion, administración y estabilidad de los siguientes medicamentos: FLUCONAZOL & VECURONIO.")
+window.alert("03/10/2023 - V 2.4.02: Se agrego prepración de la infusión continua de la insulina CTE.")

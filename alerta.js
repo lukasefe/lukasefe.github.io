89e56c2c5d64840dcc4b83ec87d4042ec4639f80
lukasefe <113307417+lukasefe@users.noreply.github.com>
2024-03-04 09:44:18 -0300

    Update alerta.js

--- alerta.js
+++ alerta.js
@@ -1,1 +1,1 @@
-window.alert("Se arreglo el problema la intentar acceder e imprimir las notas a personal como francos compensados entre otros.")
+window.alert("Se arreglo el problema al intentar acceder e imprimir las notas a personal como francos compensados entre otros.")
